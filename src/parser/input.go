587ae7ae9ac3c486450a9fe8c0fbbfda7a3c883d
Devanshu <devanshusomani99@gmail.com>
2019-02-14 22:19:56 +0530

    Identifer type problem solved

--- src/parser/input.go
+++ src/parser/input.go
@@ -1,7 +1,26 @@
-// This is a comment
 package main;
-import "fmt";
 
-func main() {
-	fmt.Println("Hello World!");
+import (
+	"fmt";
+	"strings";
+);
+
+// func main() {
+// 	// Create a tic-tac-toe board.
+// 	var game [3][3] string_t;
+//
+// 	// The players take turns.
+// 	game[0][0] = "X";
+// 	game[2][2] = "O";
+// 	game[2][0] = "X";
+// 	game[1][0] = "O";
+// 	game[0][2] = "X";
+//
+// 	printBoard(game);
+// };
+
+func printBoard(s  int) {
+	// for i := 0; i < len(s); i++ {
+	// 	f(i);
+	// };
 };
